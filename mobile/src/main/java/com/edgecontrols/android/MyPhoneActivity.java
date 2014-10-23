73bc460d65b5c782a9771958618a70159335d39e
Svetoslava Delcheva <Sve@Svetoslavas-MacBook-Pro.local>
2014-10-23 14:58:59 -0700

    If missing Wearable App and no watch connected

--- mobile/src/main/java/com/edgecontrols/android/MyPhoneActivity.java
+++ mobile/src/main/java/com/edgecontrols/android/MyPhoneActivity.java
@@ -424,6 +424,7 @@ public class MyPhoneActivity extends Activity implements GoogleApiClient.Connect
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-
+        Log.e(tag, "onConnectionFailed....");
+        showWearableNotConnected();
     }
 }
